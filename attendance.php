13e538d85e56393a10c0b69e2c9bdf0d01ca8083
Harry Li <hcli.consulting@gmail.com>
2019-02-22 12:00:02 -0500

    replacing composite primary keys with a dummy id column for the primary key. also adding a unique composite index to preserve previous functionality. these changes needed to replicate tables to Periscope.

--- attendance.php
+++ attendance.php
@@ -55,6 +55,30 @@ SET NAMES utf8mb4;
 		PRIMARY KEY(id)
 	) DEFAULT CHARACTER SET=utf8mb4;
 
+	USE braven_attendance;
+	ALTER TABLE attendance_people
+	      DROP PRIMARY KEY,
+	      ADD dummy_id INT PRIMARY KEY AUTO_INCREMENT,
+	      ADD UNIQUE INDEX unique_index (`event_id`, `person_id`);
+
+	USE braven_attendance;
+	ALTER TABLE attendance_lc_absences
+	      DROP PRIMARY KEY,
+	      ADD dummy_id INT PRIMARY KEY AUTO_INCREMENT,
+	      ADD UNIQUE INDEX unique_index (`event_id`, `lc_email`);
+
+	USE wordpress;
+	ALTER TABLE bz_term_relationships
+	      DROP PRIMARY KEY,
+	      ADD dummy_id INT PRIMARY KEY AUTO_INCREMENT,
+	      ADD UNIQUE INDEX unique_index (`object_id`, `term_taxonomy_id`);
+
+	USE wordpress;
+	ALTER TABLE wp_term_relationships
+	      DROP PRIMARY KEY,
+	      ADD dummy_id INT PRIMARY KEY AUTO_INCREMENT,
+	      ADD UNIQUE INDEX unique_index (`object_id`, `term_taxonomy_id`);
+
 	COMMIT;
 */
 
