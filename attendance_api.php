a0e72eb9813779836c50d856adb207aa988cc157
Brian Sadler <sadler.brian@gmail.com>
2019-10-12 14:48:56 -0400

    Fix attendance tracker populate_times_from_canvas() method. Handle pagination.
    The code was written to handle up to 500 sections with dates set
    for their Learning Labs per course. There can be more than 500 sections
    though. This came about b/c there is something weird going on which only
    happens in production, where the call to <portal>/api/v1/calendar_events was only
    returning 50 results per call. Now, the API call handles pagination so
    no matter how many results are returned per call, it will page through them
    all.
    
    TESTING
    - Did a dev refresh of kits and canvas from prod.
    - Hacked the lib/api.rb file in the Portal code to set a universal max of
      50 results for an api call in the self.max_per_page property.
    - Ran: cd /var/www/html; php attendance_api.php
    - Verified that Learning Lab event times were properly populated locally in the DB.
    - Verified that attendance data was sent to the Portal gradebook with the proper
      total attendance events (those in the past) count.

--- attendance_api.php
+++ attendance_api.php
@@ -4,135 +4,135 @@ date_default_timezone_set("UTC");
 
 function get_attendance_api_result($course_id, $users) {
 
-	// I want events and statuses.
+    // I want events and statuses.
 
-	$response = array("events" => get_all_events($course_id), "statuses" => array());
-	$events_in_the_past = 0;
-	foreach($response["events"] as $event) {
-		if($event["event_time"] && strtotime($event["event_time"]) < time())
-			$events_in_the_past++;
-	}
+    $response = array("events" => get_all_events($course_id), "statuses" => array());
+    $events_in_the_past = 0;
+    foreach($response["events"] as $event) {
+        if($event["event_time"] && strtotime($event["event_time"]) < time())
+            $events_in_the_past++;
+    }
 
-	foreach($response["events"] as &$event) {
-		$event["statuses"] = load_student_status($event["id"], $users)["result"];
-		foreach($event["statuses"] as $user_id => $user_status) {
-			if(!isset($response["statuses"][$user_id]))
-				$response["statuses"][$user_id] = array("events" => array(), "total_present" => 0, "total_events" => $events_in_the_past);
-			$response["statuses"][$user_id]["events"][] = $user_status;
-			if($user_status === "true" || $user_status === "late") {
-				// only if it is in the past do we want to set this count
-				if($event["event_time"] && strtotime($event["event_time"]) < time())
-					$response["statuses"][$user_id]["total_present"] += 1;
-			}
-		}
-	}
+    foreach($response["events"] as &$event) {
+        $event["statuses"] = load_student_status($event["id"], $users)["result"];
+        foreach($event["statuses"] as $user_id => $user_status) {
+            if(!isset($response["statuses"][$user_id]))
+                $response["statuses"][$user_id] = array("events" => array(), "total_present" => 0, "total_events" => $events_in_the_past);
+            $response["statuses"][$user_id]["events"][] = $user_status;
+            if($user_status === "true" || $user_status === "late") {
+                // only if it is in the past do we want to set this count
+                if($event["event_time"] && strtotime($event["event_time"]) < time())
+                    $response["statuses"][$user_id]["total_present"] += 1;
+            }
+        }
+    }
 
-	return $response;
+    return $response;
 }
 
 if(php_sapi_name() == 'cli') {
-	// if calling it from the command line, instead sync it with Canvas
-	// via the api bridges
+    // if calling it from the command line, instead sync it with Canvas
+    // via the api bridges
 
-	echo "\n*****\nRunning at " . date('r') . "\n";
+    echo "\n*****\nRunning at " . date('r') . "\n";
 
-	function set_canvas_attendance_info($course_id, $column_number, $uid, $text) {
-		$ch = curl_init();
+    function set_canvas_attendance_info($course_id, $column_number, $uid, $text) {
+        $ch = curl_init();
     $baseUrl = getPortalBaseUrl();
     $url = $baseUrl . '/api/v1/courses/'.$course_id.'/custom_gradebook_columns/'.$column_number.'/data/'.$uid;
 
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode("column_data[content]")."=".urlencode($text) . '&access_token='.urlencode(CANVAS_TOKEN));
-		$answer = curl_exec($ch);
-		curl_close($ch);
-	}
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode("column_data[content]")."=".urlencode($text) . '&access_token='.urlencode(CANVAS_TOKEN));
+        $answer = curl_exec($ch);
+        curl_close($ch);
+    }
 
-	function get_canvas_attendance_column_id($course_id) {
-		// first, try to get an existing one called Attendance and return ID
-		// and if it isn't there, go ahead and create one and return the ID
-		$ch = curl_init();
+    function get_canvas_attendance_column_id($course_id) {
+        // first, try to get an existing one called Attendance and return ID
+        // and if it isn't there, go ahead and create one and return the ID
+        $ch = curl_init();
     $baseUrl = getPortalBaseUrl();
     $url = $baseUrl . '/api/v1/courses/'.$course_id.'/custom_gradebook_columns?access_token='.urlencode(CANVAS_TOKEN);
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$answer = curl_exec($ch);
-		curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $answer = curl_exec($ch);
+        curl_close($ch);
 
-		$obj = json_decode($answer, true);
-		foreach($obj as $item) {
-			if($item["title"] == "Attendance")
-				return $item["id"];
-		}
+        $obj = json_decode($answer, true);
+        foreach($obj as $item) {
+            if($item["title"] == "Attendance")
+                return $item["id"];
+        }
 
-		// not there, create a column
+        // not there, create a column
 
-		$ch = curl_init();
+        $ch = curl_init();
     $baseUrl = getPortalBaseUrl();
     $url = $baseUrl . '/api/v1/courses/'.$course_id.'/custom_gradebook_columns';
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,
-			urlencode("column[title]")."=Attendance" .
-			'&'.urlencode("column[read_only]")."=true" .
-			'&access_token='.urlencode(CANVAS_TOKEN));
-		$answer = curl_exec($ch);
-		curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            urlencode("column[title]")."=Attendance" .
+            '&'.urlencode("column[read_only]")."=true" .
+            '&access_token='.urlencode(CANVAS_TOKEN));
+        $answer = curl_exec($ch);
+        curl_close($ch);
 
-		$obj = json_decode($answer, true);
+        $obj = json_decode($answer, true);
 
-		return $obj["id"];
-	}
+        return $obj["id"];
+    }
 
-	foreach($braven_courses as $course_id) {
+    foreach($braven_courses as $course_id) {
 
-                // Note: this is also painfully slow, but this script is only run once a week in the middle
-                // of the night from a cronjob on the Kits server, so for now it's fine.
-	        echo "  Calling: populate_times_from_canvas(course_id=".$course_id.")\n";
-                populate_times_from_canvas($course_id);
-		
-	        echo "  Calling: get_cohorts_info(course_id=".$course_id.")\n";
-                $cohort_info = get_cohorts_info($course_id);
-        
-		$column_number = get_canvas_attendance_column_id($course_id);
+        // Note: this is also painfully slow, but this script is only run once a week in the middle
+        // of the night from a cronjob on the Kits server, so for now it's fine.
+        echo "###  Calling: populate_times_from_canvas(course_id=".$course_id.")\n";
+        populate_times_from_canvas($course_id);
 
-	        echo "  Loading attendance data to send for course_id=".$course_id.")\n";
-		$result = get_attendance_api_result($course_id, $cohort_info["students"]);
+        echo "###  Calling: get_cohorts_info(course_id=".$course_id.")\n";
+        $cohort_info = get_cohorts_info($course_id);
+
+        $column_number = get_canvas_attendance_column_id($course_id);
+
+        echo "###  Loading attendance data to send for course_id=".$course_id.")\n";
+        $result = get_attendance_api_result($course_id, $cohort_info["students"]);
 
 
-                // TODO: this is brutally slow since it loops over all students in all active courses and updates the value through the API one at a time.
-                // Either figure out how to set the values in bulk or maybe check timestamps of last time 
-                // data was sent (or query canvas for last time value was updated on the canvas end)
-                // vs last time attendance status was updated and only send new ones.
-                // Or maybe just pull all attendance data from canvas in one go per course, then only send updates for changed values.
-		foreach($result["statuses"] as $uid => $status) {
-                        $attendance_data = $status["total_present"] . "/". $status["total_events"];
-	                echo "  Calling set_canvas_attendance_info(course_id=" . $course_id . ", uid=" . $uid . ", attendance=". $attendance_data .")\n";
-			set_canvas_attendance_info($course_id, $column_number, $uid, $attendance_data);
-		}
-	}
+        // TODO: this is brutally slow since it loops over all students in all active courses and updates the value through the API one at a time.
+        // Either figure out how to set the values in bulk or maybe check timestamps of last time 
+        // data was sent (or query canvas for last time value was updated on the canvas end)
+        // vs last time attendance status was updated and only send new ones.
+        // Or maybe just pull all attendance data from canvas in one go per course, then only send updates for changed values.
+        foreach($result["statuses"] as $uid => $status) {
+            $attendance_data = $status["total_present"] . "/". $status["total_events"];
+            echo "  Calling set_canvas_attendance_info(course_id=" . $course_id . ", uid=" . $uid . ", attendance=". $attendance_data .")\n";
+            set_canvas_attendance_info($course_id, $column_number, $uid, $attendance_data);
+        }
+    }
 } else {
-	// if calling via web, we do an access token chck
+    // if calling via web, we do an access token chck
 
-	if(!isset($_REQUEST["access_token"]) || $_REQUEST["access_token"] != ATTENDANCE_API_KEY) {
-		die("Unauthorized");
-	}
+    if(!isset($_REQUEST["access_token"]) || $_REQUEST["access_token"] != ATTENDANCE_API_KEY) {
+        die("Unauthorized");
+    }
 
-	// given a list of student user ids and a course...
+    // given a list of student user ids and a course...
 
-	$course_id = (int) $_REQUEST["course_id"];
-	$users = array();
-	foreach($_REQUEST["user_id"] as $uid)
-		$users[] = array("id" => (int) $uid);
+    $course_id = (int) $_REQUEST["course_id"];
+    $users = array();
+    foreach($_REQUEST["user_id"] as $uid)
+        $users[] = array("id" => (int) $uid);
 
-	$response = get_attendance_api_result($course_id, $users);
+    $response = get_attendance_api_result($course_id, $users);
 
-	header("Content-Type: application/json; charset=utf-8");
+    header("Content-Type: application/json; charset=utf-8");
 
-	echo json_encode($response);
+    echo json_encode($response);
 }
